fcea318974b753bc8fa80c18b0c541ad42fa71f0
franco-extreme <franck_ral@hotmail.com>
2018-04-22 20:22:52 -0400

    ngMaps-Material: Creating markers dynamically.

--- src/app/components/maps/maps.component.ts
+++ src/app/components/maps/maps.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Marcador } from '../../classes/marcador.class';
 
 @Component({
   selector: 'app-maps',
@@ -11,12 +12,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapsComponent implements OnInit {
 
+  markers: Marcador[] = [];
+
   lat = 51.678418;
   lng = 7.809007;
 
-  constructor() { }
+  constructor() {
+
+    const newMarker = new Marcador(51.678418, 7.809007);
+
+    this.markers.push(newMarker);
+  }
 
   ngOnInit() {
+  }
+
+  addMarker(event) {
+    // console.log(event);
+    const latitud = event.coords.lat;
+    const longitud = event.coords.lng;
+
+    const addedMarker = new Marcador(latitud, longitud);
+    this.markers.push(addedMarker);
   }
 
 }
